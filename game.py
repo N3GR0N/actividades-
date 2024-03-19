6067149562f8fb6185c8ae3346614ce73621f3b7
juan Pedro suner <sunerjuan240@gmail.com>
2024-03-19 20:14:06 -0300

    tercera modificacion niveles de dificultad agregados

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import random
 
 # Lista de palabras posibles
-words = ["python", "programación", "computadora", "código", "desarrollo", "inteligencia"]
+words = ["python", "programacion", "computadora", "código", "desarrollo", "inteligencia"]
 
 # Elegir una palabra al azar
 secret_word = random.choice(words)
@@ -11,10 +11,45 @@ max_errors = 5
 
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
+letters=[]
+vocales = "aeiou"
+
+def choose_dificulty(secret_word):
+    dificulty  = input("ingrese la dificultad (facil,normal,dificil): ").lower()
+    #si es dificil salgo de mi funcion ya que word_displayed lo genero fuera de esta 
+    if dificulty == "dificil":
+        return 
+
+    elif dificulty == "facil":
+        for letra in secret_word:
+            if letra in vocales:
+                guessed_letters.append(letra)
+    
+    elif dificulty == "normal":
+        #se agregan la primera y la ultima y en caso de que alguna de estas dos  se repita tambien se mostrara en la palabra 
+        guessed_letters.append(secret_word[0]) 
+        guessed_letters.append(secret_word[-1]) 
+        
+         
+
+    else: 
+        #sucede lo mismo que en dificil al seleccionar dificil por defecto salgo y genero el word_displayed fuera de la funcion
+        print('dificultad no valida, seleccionando dificultad dificil por defecto.')
+        return 
+    
+    
 
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
-word_displayed = "_" * len(secret_word)
+choose_dificulty(secret_word) #llamo a mi funcion para elegir una dificultad 
+
+#genero la palabra parcialmente adivinada con sus cambios por dificultad
+for letter in secret_word:
+        if letter in guessed_letters:
+            letters.append(letter)
+        else:
+            letters.append("_")
+word_displayed = "".join(letters)
 
 # Mostrar la palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
@@ -37,6 +72,7 @@ while errors < max_errors:
     else:
         # Agregar la letra a la lista de letras adivinadas
         guessed_letters.append(letter)
+       
 
         # Verificar si la letra está en la palabra secreta y contar errores si la letra no está en la palabra
         if letter in secret_word:
@@ -44,9 +80,10 @@ while errors < max_errors:
         else:
             print("Lo siento, la letra no está en la palabra.")
             errors = errors +  1
+    
 
     # Mostrar la palabra parcialmente adivinada
-    letters = []
+    letters = [] 
     for letter in secret_word:
         if letter in guessed_letters:
             letters.append(letter)
